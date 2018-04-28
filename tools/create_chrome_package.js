31692ed77436ab5ba065045075353936a7d36880
x0a <x0a.cake@gmail.com>
2018-04-28 14:32:14 -0500

    editor-friendly cleanup

--- tools/create_chrome_package.js
+++ tools/create_chrome_package.js
@@ -7,27 +7,27 @@ fs.ensureDirSync(documentRoot + "\\dist\\chrome\\debug");
 
 process.chdir(documentRoot + "\\dist\\chrome");
 fs.readFile(documentRoot + "\\shared\\manifest.json", "utf-8", function(err, contents){
-  var manifest = JSON.parse(contents);
-  delete manifest["applications"];
-  if(fs.existsSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json"))
-    fs.unlinkSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json");
-  fs.writeFileSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json", JSON.stringify(manifest, null, 4), "utf8");
+	var manifest = JSON.parse(contents);
+	delete manifest["applications"];
+	if(fs.existsSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json"))
+		fs.unlinkSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json");
+	fs.writeFileSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json", JSON.stringify(manifest, null, 4), "utf8");
 
-  var dateAndTime = new Date();
-  var fileName = documentRoot + "\\dist\\chrome\\" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".zip";
-  if(fs.existsSync(fileName)){
-    if(!fs.unlinkSync(fileName)){
-      console.log("Error removing old zip");
-    }
-  }
+	var dateAndTime = new Date();
+	var fileName = documentRoot + "\\dist\\chrome\\" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".zip";
+	if(fs.existsSync(fileName)){
+		if(!fs.unlinkSync(fileName)){
+			console.log("Error removing old zip");
+		}
+	}
 
-  fs.copySync(documentRoot + "\\src\\", documentRoot + "\\dist\\chrome\\debug\\", {overwrite: true});
-  fs.copySync(documentRoot + "\\lib\\", documentRoot + "\\dist\\chrome\\debug\\", {overwrite: true});
+	fs.copySync(documentRoot + "\\src\\", documentRoot + "\\dist\\chrome\\debug\\", {overwrite: true});
+	fs.copySync(documentRoot + "\\lib\\", documentRoot + "\\dist\\chrome\\debug\\", {overwrite: true});
 
-  exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\chrome\\debug\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
-    if(err)
-      console.log("Error creating zip: ", err);
+	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\chrome\\debug\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
+		if(err)
+			console.log("Error creating zip: ", err);
 
-  });
+	});
 })
 
--- tools/create_webext_package.js
+++ tools/create_webext_package.js
@@ -5,26 +5,26 @@ var documentRoot = path.resolve("..");
 
 
 if(!fs.existsSync(documentRoot + "\\dist\\webext"))
-  fs.mkdirSync(documentRoot + "\\dist\\webext");
+	fs.mkdirSync(documentRoot + "\\dist\\webext");
 process.chdir(documentRoot + "\\dist\\webext");
 fs.readFile(documentRoot + "\\shared\\manifest.json", "utf-8", function(err, contents){
-  var manifest = JSON.parse(contents);
-  if(fs.existsSync(documentRoot + "\\dist\\webext\\manifest.json"))
-    fs.unlinkSync(documentRoot + "\\dist\\webext\\manifest.json");
-  fs.writeFileSync(documentRoot + "\\dist\\webext\\manifest.json", JSON.stringify(manifest, null, 4), "utf8");
+	var manifest = JSON.parse(contents);
+	if(fs.existsSync(documentRoot + "\\dist\\webext\\manifest.json"))
+		fs.unlinkSync(documentRoot + "\\dist\\webext\\manifest.json");
+	fs.writeFileSync(documentRoot + "\\dist\\webext\\manifest.json", JSON.stringify(manifest, null, 4), "utf8");
 
-  var dateAndTime = new Date();
-  var fileName = documentRoot + "\\dist\\webext\\" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".xpi";
-  if(fs.existsSync(fileName)){
-    if(!fs.unlinkSync(fileName)){
-      console.log("Error removing old zip");
-    }
-  }
-  exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\webext\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
-    if(err)
-      console.log("Error creating zip: ", err);
-    fs.unlinkSync(documentRoot + "\\dist\\webext\\manifest.json");
-  });
+	var dateAndTime = new Date();
+	var fileName = documentRoot + "\\dist\\webext\\" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".xpi";
+	if(fs.existsSync(fileName)){
+		if(!fs.unlinkSync(fileName)){
+			console.log("Error removing old zip");
+		}
+	}
+	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\webext\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
+		if(err)
+			console.log("Error creating zip: ", err);
+		fs.unlinkSync(documentRoot + "\\dist\\webext\\manifest.json");
+	});
 
 })
 
