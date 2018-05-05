3022fbae03d7f081862aff5b73d1d01848ab5e9e
x0a <x0a.cake@gmail.com>
2018-05-05 02:59:09 -0500

    create debug directory for Firefox

--- tools/create_webext_package.js
+++ tools/create_webext_package.js
@@ -3,9 +3,8 @@ var path = require("path");
 var exec = require('child_process').exec;
 var documentRoot = path.resolve("..");
 
+fs.ensureDirSync(documentRoot + "\\dist\\webext\\debug");
 
-if(!fs.existsSync(documentRoot + "\\dist\\webext"))
-	fs.mkdirSync(documentRoot + "\\dist\\webext");
 process.chdir(documentRoot + "\\dist\\webext");
 fs.readFile(documentRoot + "\\shared\\manifest.json", "utf-8", function(err, contents){
 	var manifest = JSON.parse(contents);
@@ -20,6 +19,10 @@ fs.readFile(documentRoot + "\\shared\\manifest.json", "utf-8", function(err, con
 			console.log("Error removing old zip");
 		}
 	}
+
+	fs.copySync(documentRoot + "\\src\\", documentRoot + "\\dist\\webext\\debug\\", {overwrite: true});
+	fs.copySync(documentRoot + "\\lib\\", documentRoot + "\\dist\\webext\\debug\\", {overwrite: true});
+
 	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\webext\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
 		if(err)
 			console.log("Error creating zip: ", err);
