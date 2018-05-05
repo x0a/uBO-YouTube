bc9639e8dab83ca7baa303dd2b07da2390d25507
x0a <x0a.cake@gmail.com>
2018-05-05 15:27:48 -0500

    add debug folder for FF

--- tools/create_webext_package.js
+++ tools/create_webext_package.js
@@ -1,4 +1,4 @@
-var fs = require("fs");
+var fs = require("fs-extra");
 var path = require("path");
 var exec = require('child_process').exec;
 var documentRoot = path.resolve("..");
@@ -8,9 +8,9 @@ fs.ensureDirSync(documentRoot + "\\dist\\webext\\debug");
 process.chdir(documentRoot + "\\dist\\webext");
 fs.readFile(documentRoot + "\\shared\\manifest.json", "utf-8", function(err, contents){
 	var manifest = JSON.parse(contents);
-	if(fs.existsSync(documentRoot + "\\dist\\webext\\manifest.json"))
-		fs.unlinkSync(documentRoot + "\\dist\\webext\\manifest.json");
-	fs.writeFileSync(documentRoot + "\\dist\\webext\\manifest.json", JSON.stringify(manifest, null, 4), "utf8");
+	if(fs.existsSync(documentRoot + "\\dist\\webext\\debug\\manifest.json"))
+		fs.unlinkSync(documentRoot + "\\dist\\webext\\debug\\manifest.json");
+	fs.writeFileSync(documentRoot + "\\dist\\webext\\debug\\manifest.json", JSON.stringify(manifest, null, 4), "utf8");
 
 	var dateAndTime = new Date();
 	var fileName = documentRoot + "\\dist\\webext\\" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".xpi";
@@ -23,10 +23,9 @@ fs.readFile(documentRoot + "\\shared\\manifest.json", "utf-8", function(err, con
 	fs.copySync(documentRoot + "\\src\\", documentRoot + "\\dist\\webext\\debug\\", {overwrite: true});
 	fs.copySync(documentRoot + "\\lib\\", documentRoot + "\\dist\\webext\\debug\\", {overwrite: true});
 
-	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\webext\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
+	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\webext\\debug\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
 		if(err)
 			console.log("Error creating zip: ", err);
-		fs.unlinkSync(documentRoot + "\\dist\\webext\\manifest.json");
 	});
 
 })
