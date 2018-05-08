1eb561f57ce3d9e2b59f335407b948a7bd0513cc
x0a <x0a.cake@gmail.com>
2018-05-07 23:01:04 -0500

    linux-friendly path names

--- tools/create_chrome_package.js
+++ tools/create_chrome_package.js
@@ -1,42 +1,44 @@
-var fs = require("fs-extra");
-var path = require("path");
-var exec = require('child_process').exec;
-var documentRoot = path.resolve("..");
+let fs = require("fs-extra");
+let path = require("path");
+let exec = require('child_process').exec;
+let documentRoot = path.resolve("..");
 
-fs.ensureDirSync(documentRoot + "\\dist\\chrome\\debug");
+//replace JSON.parse with one that doesn't throw exception
+((parse)=>{JSON.parse=(string,reviver)=>{try{return parse(string,reviver)}catch(e){return undefined}}})(JSON.parse)
+fs.ensureDirSync(documentRoot + "/dist/chrome/debug");
 
-process.chdir(documentRoot + "\\dist\\chrome");
+//process.chdir(documentRoot + "/dist/chrome");
 let versionChanged = false;
-fs.readFile(documentRoot + "\\shared\\manifest.json", "utf-8", function(err, contents){
-	var manifest = JSON.parse(contents);
+fs.readFile(documentRoot + "/shared/manifest.json", "utf-8", function(err, contents){
+	let manifest = JSON.parse(contents);
 	delete manifest["applications"];
-	if(fs.existsSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json")){
-		let oldmanifest = JSON.parse(fs.readFileSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json").toString());
+	if(fs.existsSync(documentRoot + "/dist/chrome/debug/manifest.json")){
+		let oldmanifest = JSON.parse(fs.readFileSync(documentRoot + "/dist/chrome/debug/manifest.json").toString());
 		if(manifest.version !== oldmanifest.version)
 			versionChanged = true;
 		console.log("Current version:", manifest.version,", packaged version:", oldmanifest.version)
-		fs.unlinkSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json");
+		fs.unlinkSync(documentRoot + "/dist/chrome/debug/manifest.json");
 	}else{
 		versionChanged = true;
 	}
 	
-	fs.writeFileSync(documentRoot + "\\dist\\chrome\\debug\\manifest.json", JSON.stringify(manifest, null, 4), "utf8");
-	fs.copySync(documentRoot + "\\src\\", documentRoot + "\\dist\\chrome\\debug\\", {overwrite: true});
-	fs.copySync(documentRoot + "\\lib\\", documentRoot + "\\dist\\chrome\\debug\\", {overwrite: true});
+	fs.writeFileSync(documentRoot + "/dist/chrome/debug/manifest.json", JSON.stringify(manifest, null, 4), "utf8");
+	fs.copySync(documentRoot + "/src/", documentRoot + "/dist/chrome/debug/", {overwrite: true});
+	fs.copySync(documentRoot + "/lib/", documentRoot + "/dist/chrome/debug/", {overwrite: true});
 
 	if(versionChanged) 
 		console.log("Version changed, creating package");
 	else return;
 
 	var dateAndTime = new Date();
-	var fileName = documentRoot + "\\dist\\chrome\\" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".zip";
+	var fileName = documentRoot + "/dist/chrome/" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".zip";
 	if(fs.existsSync(fileName)){
 		if(!fs.unlinkSync(fileName)){
 			console.log("Error removing old zip");
 		}
 	}
 
-	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\chrome\\debug\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
+	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "/src/*\" \"" + documentRoot + "/dist/chrome/debug/manifest.json\" \"" + documentRoot + "/lib/*\"", "", function(err, stdout){
 		if(err)
 			console.log("Error creating zip: ", err);
 
--- tools/create_webext_package.js
+++ tools/create_webext_package.js
@@ -1,41 +1,42 @@
-var fs = require("fs-extra");
-var path = require("path");
-var exec = require('child_process').exec;
-var documentRoot = path.resolve("..");
+let fs = require("fs-extra");
+let path = require("path");
+let exec = require('child_process').exec;
+let documentRoot = path.resolve("..");
 
-fs.ensureDirSync(documentRoot + "\\dist\\webext\\debug");
+//replace JSON.parse with one that doesn't throw exception
+((parse)=>{JSON.parse=(string,reviver)=>{try{return parse(string,reviver)}catch(e){return undefined}}})(JSON.parse)
+fs.ensureDirSync(documentRoot + "/dist/webext/debug");
 
-process.chdir(documentRoot + "\\dist\\webext");
 let versionChanged = false;
-fs.readFile(documentRoot + "\\shared\\manifest.json", "utf-8", function(err, contents){
-	var manifest = JSON.parse(contents);
-	if(fs.existsSync(documentRoot + "\\dist\\webext\\debug\\manifest.json")){
-		let oldmanifest = JSON.parse(fs.readFileSync(documentRoot + "\\dist\\webext\\debug\\manifest.json").toString());
+fs.readFile(documentRoot + "/shared/manifest.json", "utf-8", function(err, contents){
+	let manifest = JSON.parse(contents);
+	if(fs.existsSync(documentRoot + "/dist/webext/debug/manifest.json")){
+		let oldmanifest = JSON.parse(fs.readFileSync(documentRoot + "/dist/webext/debug/manifest.json").toString());
 		if(manifest.version !== oldmanifest.version)
 			versionChanged = true;
 		console.log("Current version:", manifest.version,", packaged version:", oldmanifest.version)
-		fs.unlinkSync(documentRoot + "\\dist\\webext\\debug\\manifest.json");
+		fs.unlinkSync(documentRoot + "/dist/webext/debug/manifest.json");
 	}else{
 		versionChanged = true;
 	}
 		
-	fs.writeFileSync(documentRoot + "\\dist\\webext\\debug\\manifest.json", JSON.stringify(manifest, null, 4), "utf8");
-	fs.copySync(documentRoot + "\\src\\", documentRoot + "\\dist\\webext\\debug\\", {overwrite: true});
-	fs.copySync(documentRoot + "\\lib\\", documentRoot + "\\dist\\webext\\debug\\", {overwrite: true});
+	fs.writeFileSync(documentRoot + "/dist/webext/debug/manifest.json", JSON.stringify(manifest, null, 4), "utf8");
+	fs.copySync(documentRoot + "/src/", documentRoot + "/dist/webext/debug/", {overwrite: true});
+	fs.copySync(documentRoot + "/lib/", documentRoot + "/dist/webext/debug/", {overwrite: true});
 
 	if(versionChanged) 
 		console.log("Version changed, creating package");
 	else return;
 
-	var dateAndTime = new Date();
-	var fileName = documentRoot + "\\dist\\webext\\" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".xpi";
+	let dateAndTime = new Date();
+	let fileName = documentRoot + "/dist/webext/" + manifest.version + " " + dateAndTime.toLocaleString("en-us", {month: "short"}) + " " + dateAndTime.getDate() + " - " + dateAndTime.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ".xpi";
 	if(fs.existsSync(fileName)){
 		if(!fs.unlinkSync(fileName)){
 			console.log("Error removing old zip");
 		}
 	}
 
-	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "\\src\\*\" \"" + documentRoot + "\\dist\\webext\\debug\\manifest.json\" \"" + documentRoot + "\\lib\\*\"", "", function(err, stdout){
+	exec("7z a -tzip \"" + fileName + "\" \"" + documentRoot + "/src/*\" \"" + documentRoot + "/dist/webext/debug/manifest.json\" \"" + documentRoot + "/lib/*\"", "", function(err, stdout){
 		if(err)
 			console.log("Error creating zip: ", err);
 	});
