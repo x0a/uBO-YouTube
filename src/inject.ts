b2efd6fb45aa45c57534a26f7df678d039f32cd2
x0a <x0a.cake@gmail.com>
2019-06-14 20:21:38 -0500

    skip ad overlays, cleanup

--- src/inject.ts
+++ src/inject.ts
@@ -1,5 +1,6 @@
 "use strict";
 import MessageAgent from "./agent";
+import icons from "./icons";
 
 import {
     Channel, Settings, AccessURL,
@@ -125,6 +126,13 @@ class MutationWatcher {
         return false;
     }
 
+    isOverlayAd(mutation: MutationElement): HTMLButtonElement {
+        return mutation.type === "childList"
+            && mutation.target.classList.contains("ytp-ad-module")
+            && mutation.addedNodes.length
+            && mutation.target.querySelector("button.ytp-ad-overlay-close-button")
+    }
+
     isAdSkipContainer(mutation: MutationElement): HTMLElement {
         return (
             mutation.target.classList.contains("ytp-ad-skip-button-container")
@@ -137,10 +145,9 @@ class MutationWatcher {
             );
     }
 
-    adSkipButton(container: HTMLElement): HTMLElement {
+    adSkipButton(container: HTMLElement): HTMLButtonElement {
         return container.style.display !== "none"
-            && container.querySelector("button")
-            || null;
+            && container.querySelector("button");
     }
 
     pollUpdate(method: Function) {
@@ -172,39 +179,77 @@ class MutationWatcher {
             this.queuedActions.set(method, ticket)
         }
     }
-
-    findInjection(mutation: MutationElement, selector: string): void {
-        if (mutation.type === "attributes") return;
-        if (mutation.target.matches(selector)) {
-            console.log("Target is the subject of mutation", mutation)
-        } else if (mutation.type === "childList") {
-
-            for (let node of mutation.addedNodes) {
-                if (node.nodeType !== Node.ELEMENT_NODE) continue;
-
+    //** For debugging: Helps identify where/when element was added, removed, or changed */
+    findInjection(mutation: MutationRecord, selector: string) {
+        const target = mutation.target as HTMLElement;
+        if (target.matches(selector)) {
+            if (mutation.type === "attributes") {
+                console.log(
+                    `%c[${selector}].${mutation.attributeName}` +
+                    ` = %c"${mutation.oldValue}"` +
+                    ` -> %c"${target.getAttribute(mutation.attributeName || "")}"`,
+                    'font-weight: bold',
+                    'font-weight: normal; color: red;',
+                    'color: green;'
+                );
+            }
+            else {
+                console.log(`%c[${selector}] >`, 'font-weight: bold;', mutation);
+            }
+        }
+        else if (mutation.type === "childList") {
+            for (let node of mutation.addedNodes as NodeListOf<HTMLElement>) {
+                if (node.nodeType !== Node.ELEMENT_NODE)
+                    continue;
                 if (node.matches(selector)) {
-                    console.log("Subject was added under mutation", mutation);
-                } else if (node.querySelector(selector)) {
-                    console.log("Subject was added under childNode of mutation", node, "with mutation", mutation)
+                    console.log(
+                        `<${this.getSelector(target)}>.addedNodes = [...,` +
+                        `%c<${selector}>%c, ...]`, 'color: green;', mutation
+                    );
+                }
+                else if (node.querySelector(selector)) {
+                    console.log(
+                        `<${this.getSelector(target)}>.addedNodes = [...,` +
+                        `%c<${this.getSelector(node)}>%c.querySelector("%c${selector}"%c),...]`,
+                        'color: green;',
+                        '',
+                        'color: green; font-weight: bold;',
+                        mutation
+                    );
                 }
             }
-            for (let node of mutation.removedNodes) {
-                if (node.nodeType !== Node.ELEMENT_NODE) continue;
-
+            for (let node of mutation.removedNodes as NodeListOf<HTMLElement>) {
+                if (node.nodeType !== Node.ELEMENT_NODE)
+                    continue;
                 if (node.matches(selector)) {
-                    console.log("Subject was removed", mutation);
+                    console.log(
+                        `<${this.getSelector(target)}>.removedNodes = [..., <%c${selector}%c>, ...]`,
+                        'color: red'
+                    );
+                }
+                else if (node.querySelector(selector)) {
+                    console.log(
+                        `<${this.getSelector(target)}>.removedNodes = [..., ` +
+                        `%c<${this.getSelector(node)}>.querySelector("${selector}"), ...] `,
+                        'color: red'
+                    );
                 }
             }
         }
     }
-
+    getSelector(el: HTMLElement) {
+        const classes = Array.from(el.classList);
+        return el.tagName.toLowerCase()
+            + (classes.length ? "." + classes.join(".") : "")
+            + (el.id.length ? "#" + el.id : "")
+    }
     onMutation(mutations: Array<MutationElement>) {
         let mode = pages.getMode();
 
         for (let mutation of mutations) {
-            //this.findInjection(mutation, ".ytp-ad-skip-button-container");
+            this.findInjection(mutation, "canvas.ytp-tv-static");
             if (mode === VIDEO) {
-                let player, userInfo, skipContainer;
+                let player, userInfo, skipContainer, overlaySkipButton: HTMLButtonElement;
 
                 if (userInfo = this.isPolyUserInfo(mutation)) {
                     pages.video.setDataNode(userInfo)
@@ -220,8 +265,10 @@ class MutationWatcher {
                     pages.video.durationUpdate(mutation.target.textContent);
                 } else if (skipContainer = this.isAdSkipContainer(mutation)) {
                     pages.video.skipButtonUpdate(this.adSkipButton(skipContainer));
+                } else if (overlaySkipButton = this.isOverlayAd(mutation)) {
+                    overlaySkipButton.click();
                 }
-            } else if (mode === CHANNEL || mode === SEARCH || mode === ALLELSE) {
+            } else {
                 if (mode === CHANNEL) {
                     let player, skipContainer;
                     if (player = this.isPlayerUpdate(mutation)) {
@@ -335,8 +382,8 @@ class AdOptions {
         this.toggleMenu = this.toggleMenu.bind(this);
         this.lostFocus = this.lostFocus.bind(this);
 
-        this.unMuteIcon = this.generateIcon("M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zm233.32-51.08c-11.17-7.33-26.18-4.24-33.51 6.95-7.34 11.17-4.22 26.18 6.95 33.51 66.27 43.49 105.82 116.6 105.82 195.58 0 78.98-39.55 152.09-105.82 195.58-11.17 7.32-14.29 22.34-6.95 33.5 7.04 10.71 21.93 14.56 33.51 6.95C528.27 439.58 576 351.33 576 256S528.27 72.43 448.35 19.97zM480 256c0-63.53-32.06-121.94-85.77-156.24-11.19-7.14-26.03-3.82-33.12 7.46s-3.78 26.21 7.41 33.36C408.27 165.97 432 209.11 432 256s-23.73 90.03-63.48 115.42c-11.19 7.14-14.5 22.07-7.41 33.36 6.51 10.36 21.12 15.14 33.12 7.46C447.94 377.94 480 319.54 480 256zm-141.77-76.87c-11.58-6.33-26.19-2.16-32.61 9.45-6.39 11.61-2.16 26.2 9.45 32.61C327.98 228.28 336 241.63 336 256c0 14.38-8.02 27.72-20.92 34.81-11.61 6.41-15.84 21-9.45 32.61 6.43 11.66 21.05 15.8 32.61 9.45 28.23-15.55 45.77-45 45.77-76.88s-17.54-61.32-45.78-76.86z");
-        this.muteIcon = this.generateIcon("M633.82 458.1l-69-53.33C592.42 360.8 608 309.68 608 256c0-95.33-47.73-183.58-127.65-236.03-11.17-7.33-26.18-4.24-33.51 6.95-7.34 11.17-4.22 26.18 6.95 33.51 66.27 43.49 105.82 116.6 105.82 195.58 0 42.78-11.96 83.59-33.22 119.06l-38.12-29.46C503.49 318.68 512 288.06 512 256c0-63.09-32.06-122.09-85.77-156.16-11.19-7.09-26.03-3.8-33.12 7.41-7.09 11.2-3.78 26.03 7.41 33.13C440.27 165.59 464 209.44 464 256c0 21.21-5.03 41.57-14.2 59.88l-39.56-30.58c3.38-9.35 5.76-19.07 5.76-29.3 0-31.88-17.53-61.33-45.77-76.88-11.58-6.33-26.19-2.16-32.61 9.45-6.39 11.61-2.16 26.2 9.45 32.61 11.76 6.46 19.12 18.18 20.4 31.06L288 190.82V88.02c0-21.46-25.96-31.98-40.97-16.97l-49.71 49.7L45.47 3.37C38.49-2.05 28.43-.8 23.01 6.18L3.37 31.45C-2.05 38.42-.8 48.47 6.18 53.9l588.36 454.73c6.98 5.43 17.03 4.17 22.46-2.81l19.64-25.27c5.41-6.97 4.16-17.02-2.82-22.45zM32 184v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V352.6L43.76 163.84C36.86 168.05 32 175.32 32 184z")
+        this.unMuteIcon = this.generateIcon(icons.unMute);
+        this.muteIcon = this.generateIcon(icons.mute)
         this.muteButton = this.generateMenuItem(
             "Mute advertiser",
             "Automatically mute all ads from this advertiser",
@@ -347,7 +394,7 @@ class AdOptions {
         this.skipButton = this.generateMenuItem(
             "Force skip",
             "Attempt to skip this ad",
-            "M512 76v360c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12V284.1L276.5 440.6c-20.6 17.2-52.5 2.8-52.5-24.6V284.1L52.5 440.6C31.9 457.8 0 443.4 0 416V96c0-27.4 31.9-41.7 52.5-24.6L224 226.8V96c0-27.4 31.9-41.7 52.5-24.6L448 226.8V76c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12z",
+            icons.fastForward,
             () => {
                 this.closeMenu();
                 onSkip()
@@ -356,7 +403,7 @@ class AdOptions {
         this.blacklistButton = this.generateMenuItem(
             "Block advertiser",
             "Block all ads from this advertiser",
-            "M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm141.421 106.579c73.176 73.175 77.05 187.301 15.964 264.865L132.556 98.615c77.588-61.105 191.709-57.193 264.865 15.964zM114.579 397.421c-73.176-73.175-77.05-187.301-15.964-264.865l280.829 280.829c-77.588 61.105-191.709 57.193-264.865-15.964z",
+            icons.block,
             onBlacklist
         );
 
@@ -832,6 +879,7 @@ class VideoPagePoly extends SingleChannelPage {
             // or the channel changed
             // or the whitelist state changed
             // or the link changed to something that we didnt set it to
+            if (!link.href) continue;
             if (!link.channelId || link.channelId !== channelId.id || link.whitelisted !== whitelisted || link.sethref !== link.href) {
                 link.href = link.sethref = pages.reflectURLFlag(link.href, whitelisted);
                 link.whitelisted = whitelisted;
@@ -1264,6 +1312,10 @@ class Page {
         }
     }
     updateRelatedBasic(forceUpdate: boolean) {
+        // This function really doesn't work anymore as YT
+        // removed the data-ytid attribute from video elements
+        // So there's really no way to get the UCID anymore,
+        // short of wasting time chasing some Google API.
         let videos: NodeListOf<VideoBasic> = document.querySelectorAll(".video-list-item");
 
         for (let vid of videos) {
