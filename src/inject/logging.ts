02e740e7a950ffe83f0871bbd9e4855cc0d3decb
x0a <x0a.cake@gmail.com>
2020-12-14 23:00:01 -0600

    add timestamps to logs

--- src/inject/logging.ts
+++ src/inject/logging.ts
@@ -1,9 +1,16 @@
 let debug = true;
 const setDebug = (next: boolean) => debug = next;
-
+const time = () => {
+    const date = new Date();
+    const pad = (num: number) => (num + '').padStart(2, '0');
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    const seconds = pad(date.getSeconds());
+    return `${hours}:${minutes}:${seconds}`;
+}
 const log = (tag: string = 'uBO-YT-Log', ...args: Array<any>) =>
-    debug && console.log(tag, ...args);
+    debug && console.log(tag, time(), ...args);
 const err = (tag: string = 'uBO-YT-Log', ...args: Array<any>) =>
-    debug && console.error(tag, ...args);
+    debug && console.error(tag, time(), ...args);
 export { log, err, setDebug };
 export default log;
